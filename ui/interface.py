3a77d8147da3d933cafd4e7f0709470a02c2065a
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-02-11 12:55:26 +0100

    Add files via upload

---
+++ ui/interface.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+# import converte / trying to find the syntax to import prev folder
+
+def start_app():
+    root = tk.Tk()
+
+    canvas = tk.Canvas(root, width=500, height=500)
+    canvas.pack()
+
+    # title
+
+    # input for link
+
+    # input for the output folder 
+    # will be saved for the next time
+
+    # format choosing drop down
+
+    # convert button 
+    
+    # progress bar / no idea how this is gonna work but thats why we are here
+
+    # footer with social links or something legal 
+
+    root.mainloop()
