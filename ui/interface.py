f9a260c2a2830035a9277e978dfe264d16269c40
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-02-12 11:28:55 +0100

    Add files via upload

--- ui/interface.py
+++ ui/interface.py
@@ -1,8 +1,15 @@
 import tkinter as tk
+from tkinter import ttk
 # import converte / trying to find the syntax to import prev folder
+
+def do_conversion(link, path):
+    # converter(link ,path)
+    pass
 
 def start_app():
     root = tk.Tk()
+
+    root.geometry('500x500')
 
     canvas = tk.Canvas(root, width=500, height=500)
     canvas.pack()
@@ -11,13 +18,22 @@ def start_app():
 
     # input for link
 
+    link_input = ttk.Entry(canvas)
+    link_input.pack(side = tk.TOP ,ipadx = 30, ipady = 6)
+
     # input for the output folder 
     # will be saved for the next time
+
+    folder_input = ttk.Entry(canvas)
+    folder_input.pack(side = tk.TOP, ipadx = 30, ipady = 6)
 
     # format choosing drop down
 
     # convert button 
     
+    convert_button = ttk.Button(canvas, text="convert", command=lambda: do_conversion(link_input.get(), folder_input.get()))
+    convert_button.pack(side = tk.TOP, ipadx = 30, ipady = 6)
+
     # progress bar / no idea how this is gonna work but thats why we are here
 
     # footer with social links or something legal 
